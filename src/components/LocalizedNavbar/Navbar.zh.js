28a09cb53c1c044919f260153fb31733e4d2fae3
vtea <959996@gmail.com>
2023-05-16 19:50:04 +0800

    edit

--- src/components/LocalizedNavbar/Navbar.zh.js
+++ src/components/LocalizedNavbar/Navbar.zh.js
@@ -7,47 +7,77 @@ const zhNavbar = {
   },
   items: [
     {
-      to: "https://aiprm.top",
-      label: "Aiprm",
+      to: "https://chat.aiprm.top",
+      label: "Chatgpt体验",
       position: "left",
     },
     {
       type: 'dropdown',
-      label: '其他工具',
+      label: 'ChatGPT 项目',
       position: 'left',
       items: [
-        
-        {
-          label: '高级提示词',
-          href: 'https://aiprm.top/t/prm',
-        },
-        {
-          label: 'api入口',
-          href: 'https://platform.openai.com',
-        },
-        {
-          label: '官方Chat',
-          href: 'https://chat.openai.com',
-        },
         {
           label: "ChatGPT-Next-Web",
           href: "https://github.com/Yidadaa/ChatGPT-Next-Web",
         },
+
+        {
+          label: 'chatgpt-web',
+          href: 'https://github.com/Chanzhaoyu/chatgpt-web',
+        },
+        {
+          label: 'ChatGPT-Admin-Web',
+          href: 'https://github.com/AprilNEA/ChatGPT-Admin-Web',
+        },
+        {
+          label: 'chatgpt-web-plus',
+          href: 'https://github.com/chatgpt-web-plus/free',
+        },
+        
       ],
     },
     {
-      to: "https://chat.aiprm.top",
-      label: "在线体验",
+      type: 'dropdown',
+      label: 'Ai 商家',
+      position: 'left',
+      items: [
+        {
+          label: "小天的店铺",
+          href: "https://fk.xt666.cc/",
+        },
+
+        {
+          label: '汤姆的店铺',
+          href: 'https://tomfk.top/',
+        },
+        {
+          label: "店小王",
+          href: "https://store.thew.one/",
+        },
+        {
+          label: '卡商发布申请',
+          href: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=m55iSKX6PolEhjit-YcQcZ_KM3k3baFH&authKey=wJDWRCuA4A0kbUEurPWceYqan%2FmDtIwCi8ciDPlmHq2qQCIKFjg71Rk02iMISZgb&noverify=0&group_code=125800042',
+        },
+        {
+          label: '自行判断',
+          href: '#',
+        },
+        
+      ],
+    },
+    {
+      to: "https://chat.openai.com",
+      label: "Chatgpt官网",
       position: "left",
     },
     { type: "localeDropdown", position: "right" },
     {
-      href: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=8iL4Va8YaqNyrbdifhEOD0biZx8l2tWY&authKey=TNyKaORGJ0wwCuiwy6BDBlI1MnHM4JTZVJpf9uP%2BT0%2FH5QgW5yrtoK5zKXVhe8xX&noverify=0&group_code=125800040",
+      href: "https://github.com/rockbenben/ChatGPT-Shortcut",
       position: "right",
       className: "header-github-link",
     },
     {
-      href: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=8iL4Va8YaqNyrbdifhEOD0biZx8l2tWY&authKey=TNyKaORGJ0wwCuiwy6BDBlI1MnHM4JTZVJpf9uP%2BT0%2FH5QgW5yrtoK5zKXVhe8xX&noverify=0&group_code=125800040",
+      href: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=PFsjq1laajygOC3Bhb_1O8T7GpQBOtBo&authKey=Bo8Vbkz4Sa6%2BmOh%2F9wkWTOMa%2BCWpjdoa5Q4Hwk%2FqFVjO0rMnOH2yZ1HZr%2FQxO7Tn&noverify=0&group_code=299294863",
       position: "right",
       className: "header-discord-link",
     },
