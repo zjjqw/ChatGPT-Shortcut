dc073f62151bb3956506f518a626b8ffbf748997
vtea <959996@gmail.com>
2023-05-16 19:15:55 +0800

    Update Navbar.en.js

--- src/components/LocalizedNavbar/Navbar.en.js
+++ src/components/LocalizedNavbar/Navbar.en.js
@@ -14,12 +14,12 @@ const enNavbar = {
     },
     { type: "localeDropdown", position: "right" },
     {
-      href: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=8iL4Va8YaqNyrbdifhEOD0biZx8l2tWY&authKey=TNyKaORGJ0wwCuiwy6BDBlI1MnHM4JTZVJpf9uP%2BT0%2FH5QgW5yrtoK5zKXVhe8xX&noverify=0&group_code=125800040",
+      href: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=ZMoZXvWdkpPNz8OWzCsG9CMd8VRKZQAi&authKey=8RsoR%2FV8bFn2KTqMr%2BJ%2FHFznZfv%2BPgYsQgAv1QUoieJ7JhshYP0m8VpqwYpFV5N9&noverify=0&group_code=181509991",
       position: "right",
       className: "header-github-link",
     },
     {
-      href: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=8iL4Va8YaqNyrbdifhEOD0biZx8l2tWY&authKey=TNyKaORGJ0wwCuiwy6BDBlI1MnHM4JTZVJpf9uP%2BT0%2FH5QgW5yrtoK5zKXVhe8xX&noverify=0&group_code=125800040",
+      href: "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=ZMoZXvWdkpPNz8OWzCsG9CMd8VRKZQAi&authKey=8RsoR%2FV8bFn2KTqMr%2BJ%2FHFznZfv%2BPgYsQgAv1QUoieJ7JhshYP0m8VpqwYpFV5N9&noverify=0&group_code=181509991",
       position: "right",
       className: "header-discord-link",
     },
